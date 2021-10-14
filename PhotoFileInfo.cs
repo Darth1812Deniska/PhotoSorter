1c26373ce4c7cf1d269412083ce3fe6068621e52
Denis Davyddov <den93mos@yandex.ru>
2021-10-14 23:20:43 +0300

    Добавление записей в лог и логгирование

--- PhotoFileInfo.cs
+++ PhotoFileInfo.cs
@@ -5,24 +5,28 @@ namespace PhotoSorter
 {
     public class PhotoFileInfo
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         private string _fileFullPath;
         private string _fileName;
         private DateTime _createDatetime;
         private int _createYear;
         private int _createMonth;
+        private int _createDay;
 
         public string FileName { get => _fileName; set => _fileName = value; }
         public string FileFullPath { get => _fileFullPath; set => _fileFullPath = value; }
         public DateTime CreateDatetime { get => _createDatetime; set => _createDatetime = value; }
         public int CreateYear { get => _createYear; set => _createYear = value; }
         public int CreateMonth { get => _createMonth; set => _createMonth = value; }
+        public int CreateDay { get => _createDay; set => _createDay = value; }
         public string FolderToMove { get => GetFolderToMove(); }
         public string FileNameToMove { get => GetFileNameToMove(); }
+        public string DatePrefix { get => GetDatePrefix(); }
 
         public PhotoFileInfo(string fileFullPath)
         {
             _fileFullPath = fileFullPath;
-            Console.WriteLine($"Найден файл {_fileFullPath}");
+            Logger.Info($"Найден файл {_fileFullPath}");
             try
             {
                 using (ExifReader reader = new ExifReader(FileFullPath))
@@ -34,29 +38,35 @@ namespace PhotoSorter
             {
                 _createDatetime = DateTime.MinValue;
             }
-            
+
             //_createDatetime = createDateTime;
             _fileName = Path.GetFileName(FileFullPath);
-            Console.WriteLine($"Имя файла = {_fileName}, " +
+            Logger.Info($"Имя файла = {_fileName}, " +
                 $"расширение файла = {Path.GetExtension(FileFullPath)}");
             _createYear = CreateDatetime.Year;
             _createMonth = CreateDatetime.Month;
-            Console.WriteLine($"Дата создания = {CreateDatetime}, год = {CreateYear}" +
+            _createDay = CreateDatetime.Day;
+            Logger.Info($"Дата создания = {CreateDatetime}, год = {CreateYear}" +
                 $", месяц = {CreateMonth}");
         }
 
-        public string GetFolderToMove()
+        private string GetFolderToMove()
         {
-            string result = $"{CreateYear}{Path.DirectorySeparatorChar}{CreateMonth}";
+            string result = $"{CreateYear:D4}{Path.DirectorySeparatorChar}{CreateMonth:D2}";
             return result;
         }
 
-        public string GetFileNameToMove()
+        private string GetFileNameToMove()
         {
             string result = string.Empty;
             string folderToMove = GetFolderToMove();
             result = $"{folderToMove}{Path.DirectorySeparatorChar}{FileName}";
             return result;
+        }
+
+        private string GetDatePrefix()
+        {
+            return $"{CreateYear:D4}{CreateMonth:D2}{CreateDay:D2}";
         }
 
     }
